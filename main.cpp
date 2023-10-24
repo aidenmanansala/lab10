8f55be2b76ae6ef558f201ae441794e460cce023
Luke Steinford <lsteinford@nic.edu>
2023-10-24 15:42:07 -0700

    Merged main functions

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 
 #include "display.h"
+#include "sound.h"
 
 int main()
 {
     std::string message = "This is Luke's branch.";
     displayString(message);
+
+    std::cout << "This is Aiden's branch.\n";
+    playSound();
 
     return 0;
 }
