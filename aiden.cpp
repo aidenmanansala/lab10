ab7512256264756b9c0d6a272e4553dba7c8252f
aidenmanansala <aidenmanansala@proton.me>
2023-10-24 15:23:05 -0700

    plays sounds

---
+++ aiden.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "sound.h"
+
+int main() {
+    std::cout<<"Aidens file"<<std::endl;
+    playSound();
+    return 0;
+}
---
+++ sound.h
@@ -0,0 +1,8 @@
+#ifndef SOUND_H
+#define SOUND_H
+
+#include <iostream>
+
+void playSound();
+
+#endif
